7f6790e10e9a7fede7ea6592d0a7d8834ee6a6a7
rajpal-tech <rajpaltechugo@gmail.com>
2021-01-23 18:53:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import About from './About'
+import Contact from './Contact'
+import Users from './Users'
+import CreateUser from './CreateUser'
+import Home from './Home'
+import { Link, Route, BrowserRouter as Router,Switch } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+       <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link href="#features"><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link href="#pricing"><Link to="/contact">Contact</Link>
+            </Nav.Link>
+            <Nav.Link href="#pricing"><Link to="/list">User List</Link>
+            </Nav.Link> <Nav.Link href="#pricing"><Link to="/create">Create User</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+         <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/list">
+            <Users />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/create">
+            <CreateUser />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
